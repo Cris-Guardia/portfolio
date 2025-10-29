73f5031fae026285c8ddd0c5f92367a20d3480d9
Cristofer Guardia <cristofer10501@gmail.com>
2025-10-29 19:11:01 +0100

    Version 1.1.3

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,7 @@ import './About.css'
 function About(){
     return(
         <main className='about'>
-            <h2>À propos de moi</h2>
+            <h2>À propos de moi.</h2>
 
             <p>
                 Vénézuélien, étudiant international et autonome, je suis actuellement en deuxième année de BUT GEII.
@@ -33,16 +33,16 @@ function About(){
             </p>
 
             <section>
-                <h3>Études</h3>
+                <h3>Études.</h3>
 
                 <div className='ul-container'>
                     <ul>
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>À partir de septembre 2025</h6>
+                                <h6>À partir de septembre 2025.</h6>
                             </span>
-                            <h4>I.U.T de Gradignan</h4>
+                            <h4>I.U.T de Gradignan.</h4>
                             <p>
                                 Actuellement en deuxième année de BUT Génie Électrique et Informatique Industrielle (GEII),
                                 avec une spécialisation en Automatisme et Informatique Industrielle.
@@ -51,9 +51,9 @@ function About(){
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>Septembre 2024 - Juin 2025</h6>
+                                <h6>Septembre 2024 - Juin 2025.</h6>
                             </span>
-                            <h4>I.U.T de Tarbes</h4>
+                            <h4>I.U.T de Tarbes.</h4>
                             <p>
                                 Formation en Génie Électrique et Informatique Industrielle (GEII)
                                 axée sur les systèmes automatisés, l'électronique et la programmation industrielle.
@@ -62,9 +62,9 @@ function About(){
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>Janvier 2023 - Juin 2024</h6>
+                                <h6>Janvier 2023 - Juin 2024.</h6>
                             </span>
-                            <h4>Campus Langues, Paris</h4>
+                            <h4>Campus Langues, Paris.</h4>
                             <p>
                                 Étude de la langue française, acquérant des compétences linguistiques solides et une bonne maîtrise à l'écrit comme à l'oral.
                             </p>
@@ -72,11 +72,11 @@ function About(){
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>Janvier 2021 - Décembre 2022</h6>
+                                <h6>Janvier 2021 - Décembre 2022.</h6>
                             </span>
-                            <h4>UNET (Au Venezuela)</h4>
+                            <h4>UNET (Au Venezuela).</h4>
                             <p>
-                                Ingénierie en informatique et développement de logiciels.
+                                Bac +1 en Informatique et Programmation.
                             </p>
                         </li>
                     </ul>
@@ -84,16 +84,16 @@ function About(){
             </section>
 
             <section>
-                <h3>Expériences Personnelles</h3>
+                <h3>Expériences Personnelles.</h3>
 
                 <div className='ul-container'>
                     <ul>
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>À partir de Aôut 2025</h6>
+                                <h6>À partir de Aôut 2025.</h6>
                             </span>
-                            <h4>McDonald's, Merignac</h4>
+                            <h4>McDonald's, Merignac.</h4>
                             <p>
                                 Employé polyvalent responsable de la préparation des plats et du nettoyage.
                             </p>
@@ -101,9 +101,9 @@ function About(){
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>À partir de décembre 2024</h6>
+                                <h6>Novembre 2024 - Juin 2025.</h6>
                             </span>
-                            <h4>McDonald's, Tarbes</h4>
+                            <h4>McDonald's, Tarbes.</h4>
                             <p>
                                 Employé polyvalent responsable de la préparation des plats et du nettoyage.
                             </p>
@@ -111,9 +111,9 @@ function About(){
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>Août 2023 - Août 2024</h6>
+                                <h6>Août 2023 - Août 2024.</h6>
                             </span>
-                            <h4>Restaurant Green Deli, Paris</h4>
+                            <h4>Restaurant Green Deli, Paris.</h4>
                             <p>
                                 Employé polyvalent responsable de la préparation des plats, 
                                 le service aux clients, le nettoyage et l’entretien du restaurant.
@@ -122,9 +122,9 @@ function About(){
                         <li>
                             <span>
                                 <CiCalendar />
-                                <h6>Avril 2023 - Août 2023</h6>
+                                <h6>Avril 2023 - Août 2023.</h6>
                             </span>
-                            <h4>Restaurant Totuma, Paris</h4>
+                            <h4>Restaurant Totuma, Paris.</h4>
                             <p>
                                 En charge de l'accueil des clients, de la prise de commande et de l'élaboration des boissons. 
                                 Soutien de l'équipe en cuisine pour la préparation des plats.
---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,27 @@
+// [
+//     {
+//         "title": "Projet d’études : Conception d’un système de pilotage pour robot",
+//         "description":  "Projet de programmation visant à créer un logiciel capable de guider un robot sur une piste afin de suivre une
+//                         ligne blanche posée au sol. Par ailleurs, cette piste présentait divers obstacles au robot et,
+//                         en utilisant des capteurs infrarouges et des capteurs à ultrasons, le robot était capable de détecter les obstacles
+//                         présents devant lui.
+//                         La piste sur laquelle le robot évoluait était de forme carrée, avec un capteur à chaque coin. Le robot
+//                         disposait d’un capteur qui mesurait la distance entre lui et chaque capteur placé aux coins, lui permettant de
+//                         connaître sa position précise.
+//                         \n
+//                         \n
+//                         Le logiciel a été créé en Python avec l’outil MicroPython, qui a permis d’utiliser une carte Pyboard, chargée
+//                         de gérer les composants tels que les capteurs ou les moteurs.
+//                         \n
+//                         \n
+//                         D’ailleurs, le projet a été développé en groupe de trois. Le travail à réaliser a été divisé en plusieurs tâches
+//                         plus petites, ce qui a permis de réduire sa complexité et de gérer le projet de manière plus efficace."
+                
+                        
+                
+//     },
+//     {},
+//     {},
+//     {},
+//     {}
+// ]
